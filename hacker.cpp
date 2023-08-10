00a7b1a64ca784f39f7b9689a244e5d2e50f2688
parth910f <140676230+parth910f@users.noreply.github.com>
2023-08-10 10:52:15 +0530

    Add files via upload

--- hacker.cpp
+++ hacker.cpp
@@ -3,170 +3,39 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-	if(n>0 and n<10 and n%2!=0){
-        if(n==1){
-            for(int i=1;i<=5;++i){
-                cout<<"*"<<'\t';
-            }cout<<endl;
+	if(n<=20){
+        for(int i=1;i<=n;++i){
+            if(i==1){
+                for(int j=1;j<=n-1;++j){
+                    cout<<" ";
+                }
+                for(int j=1;j<=n;++j){
+                    cout<<"*";
+                }cout<<endl;
+            }
+            if(i!=n and i!=1){
+                for(int j=1;j<=(n-i);++j){
+                   cout<<" ";
+                }                
+               for(int j=1; j<=1; ++j){
+                   cout<<"*";
+               }for(int j=1; j<=n-2; ++j){
+                   cout<<" ";
+               }for(int j=1;j<=1;++j){
+                   cout<<"*";
+               } 
+               cout<<endl;
+            }
+            if(i==n){
+                for(int j=1;j<=n;++j){
+                    cout<<"*";
+                }
+
+            }
         }
-        else if(n==3){
-            for(int i=1;i<=5;++i){
-                cout<<"*"<<'\t';
-            }cout<<endl;
-            for(int i=1; i<=2; ++i){
-                for(int j=1; j<=(3-i);++j){
-                    cout<<"*"<<'\t';
-                }
-                for(int j=1;j<=(2*i-1);++j){
-                    cout<<" "<<'\t';
-                }
-                for(int j=1;j<=(3-i);++j){
-                    cout<<"*"<<'\t';
-                }
-                cout<<endl;
-            }}
-            else if(n==5){
-
-               for(int i=1;i<=5;++i){
-                   cout<<"*"<<'\t';
-                 }cout<<endl;
-               for(int i=1; i<=2; ++i){
-                    for(int j=1; j<=(3-i);++j){
-                       cout<<"*"<<'\t';
-                 }
-                    for(int j=1;j<=(2*i-1);++j){
-                       cout<<" "<<'\t';
-                     }
-                     for(int j=1;j<=(3-i);++j){
-                        cout<<"*"<<'\t';
-                    }
-                     cout<<endl;
-                     }
-               for(int i=4;i<=5;++i){
-                   for(int j=1;j<=(i-2);++j){
-                       cout<<"*"<<'\t';
-                   }
-                   for(int j=1; j<=(5-i);++j){
-                       cout<<" "<<'\t';
-                    }
-                   for(int j=1; j<=2;++j){
-                       cout<<"*"<<'\t';
-                   }
-                   cout<<endl;
-                   }
-            }
-            else if(n==7){
-
-               for(int i=1;i<=5;++i){
-                   cout<<"*"<<'\t';
-                 }cout<<endl;
-               for(int i=1; i<=2; ++i){
-                    for(int j=1; j<=(3-i);++j){
-                       cout<<"*"<<'\t';
-                 }
-                    for(int j=1;j<=(2*i-1);++j){
-                       cout<<" "<<'\t';
-                     }
-                     for(int j=1;j<=(3-i);++j){
-                        cout<<"*"<<'\t';
-                    }
-                     cout<<endl;
-                     }
-               for(int i=4;i<=5;++i){
-                   for(int j=1;j<=(i-2);++j){
-                       cout<<"*"<<'\t';
-                   }
-                   for(int j=1; j<=(5-i);++j){
-                       cout<<" "<<'\t';
-                    }
-                   for(int j=1; j<=2;++j){
-                       cout<<"*"<<'\t';
-                   }
-                   cout<<endl;
-                   }
-               for(int i=1;i<=5;++i){
-                   cout<<"*"<<'\t';
-                 }cout<<endl;
-               for(int i=1; i<=1; ++i){
-                    for(int j=1; j<=(3-i);++j){
-                       cout<<"*"<<'\t';
-                    }
-                    for(int j=1;j<=(2*i-1);++j){
-                       cout<<" "<<'\t';
-                     }
-                     for(int j=1;j<=(3-i);++j){
-                        cout<<"*"<<'\t';
-                    }
-                     cout<<endl;
-                     }
-                   
-
-            }
-            else if(n==9){
-
-               for(int i=1;i<=5;++i){
-                   cout<<"*"<<'\t';
-                 }cout<<endl;
-               for(int i=1; i<=2; ++i){
-                    for(int j=1; j<=(3-i);++j){
-                       cout<<"*"<<'\t';
-                 }
-                    for(int j=1;j<=(2*i-1);++j){
-                       cout<<" "<<'\t';
-                     }
-                     for(int j=1;j<=(3-i);++j){
-                        cout<<"*"<<'\t';
-                    }
-                     cout<<endl;
-                     }
-               for(int i=4;i<=5;++i){
-                   for(int j=1;j<=(i-2);++j){
-                       cout<<"*"<<'\t';
-                   }
-                   for(int j=1; j<=(5-i);++j){
-                       cout<<" "<<'\t';
-                    }
-                   for(int j=1; j<=2;++j){
-                       cout<<"*"<<'\t';
-                   }
-                   cout<<endl;
-                   }
-               for(int i=1;i<=5;++i){
-                   cout<<"*"<<'\t';
-                 }cout<<endl;
-               for(int i=1; i<=2; ++i){
-                    for(int j=1; j<=(3-i);++j){
-                       cout<<"*"<<'\t';
-                    }
-                    for(int j=1;j<=(2*i-1);++j){
-                       cout<<" "<<'\t';
-                     }
-                     for(int j=1;j<=(3-i);++j){
-                        cout<<"*"<<'\t';
-                    }
-                     cout<<endl;
-                    }
-               for(int i=4;i<=4;++i){
-                   for(int j=1;j<=(i-2);++j){
-                       cout<<"*"<<'\t';
-                   }
-                   for(int j=1; j<=(5-i);++j){
-                       cout<<" "<<'\t';
-                    }
-                   for(int j=1; j<=2;++j){
-                       cout<<"*"<<'\t';
-                   }
-                   cout<<endl;
-                   }    
-
-                 
 
 
-
-            }    
-        
-        
-    }
+        }
 
 	else{
 		cout<<"wrong Input Entered";
